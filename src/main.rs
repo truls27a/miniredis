855a04a528dc274a17176d78afb080bee2319752
truls27a <truls369@gmail.com>
2025-07-28 18:05:37 +0200

    feat: set up multithreaded simulation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::{sync::Arc, thread};
+use miniredis::kv_store::KVStore;
+
 fn main() {
-    println!("Hello, world!");
+    let store = Arc::new(KVStore::new());
+
+    let mut handles = Vec::new();
+
+    for _ in 0..10 {
+        let store = store.clone();
+        let handle = thread::spawn(move || {
+            store.set("key", "value");
+            println!("set key for thread {:?}", thread::current().id());
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("All threads done!");
 }
